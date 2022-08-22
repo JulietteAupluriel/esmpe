4d5b022f6a51f446ab4bda67fcc7db87fca79921
Juliette <juliette@aupluriel.be>
2022-08-22 09:21:06 +0200

    logo store

--- app/Http/Controllers/LogoController.php
+++ app/Http/Controllers/LogoController.php
@@ -29,7 +29,7 @@ class LogoController extends Controller
     }
 
     public function store(Logo $logo) {
-         Logo::create($this->data());
+        Logo::create($this->data());
         $this->image($logo);
         return redirect()->route('admin.logos');
     }
