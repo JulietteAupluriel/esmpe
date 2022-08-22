edebb7110f15588690e1fe80e9cdf314dcb479ff
Juliette <juliette@aupluriel.be>
2022-08-22 09:00:01 +0200

    storage

--- app/Http/Controllers/LogoController.php
+++ app/Http/Controllers/LogoController.php
@@ -53,7 +53,7 @@ class LogoController extends Controller
             'url_fr' => 'filled|string|max:255',
             'url_nl' => 'filled|string|max:255',
             'page' => 'filled|string|max:255',
-            'order' => 'number',
+            'order' => 'integer',
             'image' => 'filled'
             
             
