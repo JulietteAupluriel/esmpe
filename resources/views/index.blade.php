6469c44a6970da99593eaecc33c71d5744065575
Juliette <juliette@aupluriel.be>
2022-09-02 17:15:38 +0200

    lang nl

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,8 +19,7 @@
         <div class="intro"><p>{!! $content->intro_fr !!}</p>
         @else
           
-        <h1>Etterbeek zet zich 
-            Etterbeek mobiliseert in<br/><b><span>voor werk</span></b></h1>
+        <h1>Etterbeek mobiliseert in <b><span>voor werk</span></b></h1>
         <div class="intro"><p>{!! $content->intro_nl !!}</p>
         @endif
 
