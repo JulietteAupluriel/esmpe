c7fa04d11cc8d21b07a098b01131d02b28c17ed6
Reza ODB <reza@aupluriel.be>
2022-09-21 17:41:49 +0200

    debug export CSV

--- routes/auth.php
+++ routes/auth.php
@@ -28,13 +28,15 @@ Route::middleware('guest')->group(function () {
 
     Route::post('reset-password', [NewPasswordController::class, 'store'])
                 ->name('password.update');
-});
 
-Route::middleware('auth')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
     Route::post('register', [RegisteredUserController::class, 'store']);
+});
+
+Route::middleware('auth')->group(function () {
+    
 
     Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
                 ->name('verification.notice');
